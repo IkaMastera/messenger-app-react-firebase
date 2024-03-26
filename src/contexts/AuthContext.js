e2a7cdc03d34f74300eeeabcacb3add6bd8e57dd
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-03-26 18:17:37 +0400

    add: auth Context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -6,4 +6,24 @@ const AuthContext = React.createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
-export const AuthProvider = ({ children }) => {};
+export const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({});
+  const history = useHistory();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+      history.push("/chats");
+    });
+  }, [user, history]);
+
+  const value = { user };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
