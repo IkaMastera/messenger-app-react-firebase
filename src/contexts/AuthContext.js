2df1a8a1f614a89dc3d3b468de4e8cef0c82990c
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-03-26 18:24:44 +0400

    set: initial state of user to null
    also added chats component and tested the google and facebook logins everything working !

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import useHistory from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { auth } from "../firebase";
 
 const AuthContext = React.createContext();
@@ -8,14 +8,14 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const history = useHistory();
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       setUser(user);
       setLoading(false);
-      history.push("/chats");
+      if (user) history.push("/chats");
     });
   }, [user, history]);
 
